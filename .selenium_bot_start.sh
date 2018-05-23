72dd36149696e33992be719ffe2c1ffa889c3030
xXuA254aFeR8BVdi <9DJ6sYRknT7aAuf8@protonmail.com>
2018-05-24 00:43:48 +0900

    ready for test

---
+++ .selenium_bot_start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /home/alexander/miniconda3/bin/activate selenium_user_bot
+cd /home/alexander/python_projects/selenium_bot/
+export PATH=$PATH:/home/alexander/geckodriver/
+/home/alexander/miniconda3/bin/python selenium_bot.py
